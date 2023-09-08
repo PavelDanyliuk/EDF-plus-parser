92b65458a7776e0890c2adf54bba60f3591045e3
Pavlo Danyliuk <pavlo.danyliuk@bresotec.com>
2023-09-08 10:24:46 -0600

    :bug: array-like headers parsed correctly

--- services/headers_test.go
+++ services/headers_test.go
@@ -1,23 +1,34 @@
 package services
 
 import (
+	"bufio"
 	"os"
+	"reflect"
 	"testing"
 )
 
 const filePath string = "../content/example_small.edf"
 
-func TestInitReader(t *testing.T) {
+func TestParseHeaders(t *testing.T) {
 	file, err := os.Open(filePath)
 	if err != nil {
 		t.Errorf("Can not read the file")
 	}
+
 	defer file.Close()
 
-	reader := InitReader(file)
+	bufioReader := bufio.NewReader(file)
+	headers := &Headers{}
+
+	headers.Parse(bufioReader)
 
 	expectedSignals := "5"
-	if reader.Headers.NumberOfSignals != expectedSignals {
-		t.Errorf("Reader.signals = %s, expected %s", reader.Headers.NumberOfSignals, expectedSignals)
+	if headers.NumberOfSignals != expectedSignals {
+		t.Errorf("headers.NumberOfSignals = %s, expected %s", headers.NumberOfSignals, expectedSignals)
+	}
+
+	expectedLabels := []string{"SPO2", "Heart Rate", "Effort", "Flow", "EDF Annotations"}
+	if !reflect.DeepEqual(headers.Labels, expectedLabels) {
+		t.Errorf("headers.Labels = %s, expected %s", headers.Labels, expectedLabels)
 	}
 }
